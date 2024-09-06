fe3e41d6ace9913f922b63f3311dcef4a19d5835
Sergey Novikov <sergey.novikov@flaconi.de>
2024-09-06 12:38:23 +0200

    Remove reference to deprecate value field

--- modules/record_collection/outputs.tf
+++ modules/record_collection/outputs.tf
@@ -1,4 +1,4 @@
 output "values" {
   description = "Cloudflare Zone DNS Records"
-  value       = { for v in cloudflare_record.this : contains(keys(v), "value") ? v.value : v.data.value => { id = v.id, created = v.created_on } }
+  value       = { for v in cloudflare_record.this : contains(keys(v), "content") ? v.content : v.data.value => { id = v.id, created = v.created_on } }
 }
