c4afd21a51efa791d34f1b15cd05543f4d75e12b
Sergey Novikov <sergey.novikov@flaconi.de>
2025-02-11 18:46:12 +0100

    Pin cloudflare provider version 4.52

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.41"
+      version = "4.52.0"
     }
   }
   required_version = "~> 1.3"
